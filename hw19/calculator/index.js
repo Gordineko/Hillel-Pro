37cb3882dcd40e88f69eb3ed85fd6e591bfdc039
Danil Gordienko <2018danil@gmail.com>
2022-11-14 18:48:07 +0200

    HW 20

---
+++ hw19/calculator/index.js
@@ -0,0 +1,11 @@
+const sum = require("./sum");
+const min = require("./min");
+const div = require("./div");
+const mult = require("./mult");
+
+module.exports = {
+  sum,
+  min,
+  div,
+  mult,
+};
---
+++ hw19/index.js
@@ -0,0 +1,6 @@
+const calculator = require("./calculator");
+
+console.log(calculator.sum(2, 10));
+console.log(calculator.min(12, 5));
+console.log(calculator.mult(4, 7));
+console.log(calculator.div(28, 2));
